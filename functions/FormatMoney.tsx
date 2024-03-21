0096bc5f8c88a748c85c075de18c5762e049159a
Eric Phifer <ericoncode@gmail.com>
2024-03-21 07:17:10 -0600

    try new code for FormatMoney

--- functions/FormatMoney.tsx
+++ functions/FormatMoney.tsx
@@ -6,57 +6,57 @@ const formatMoney = Intl.NumberFormat('en-US', {
   currency: 'USD',
 }).format;
 
-// export default function FormatMoney(props: StringInputProps) {
-//   const { onChange, value: inputValue = '', elementProps } = props;
-
-//   const handleChange = useCallback(
-//     (event: React.ChangeEvent<HTMLInputElement>) => {
-//       const newValue = event.currentTarget.value.trim();
-//       if (newValue === '') {
-//         onChange(unset());
-//       } else {
-//         const parsedValue = parseInt(newValue, 10);
-//         if (!isNaN(parsedValue)) {
-//           onChange(set(parsedValue));
-//         }
-//       }
-//     },
-//     [onChange]
-//   );
-
-//   return (
-//     <div>
-//       <h2>In US Dollars - {inputValue ? formatMoney(inputValue / 100) : null}</h2>
-//       <p>Enter the amount, with cents and no decimal. It will auto convert above.</p>
-//       <input
-//         {...elementProps}
-//         value={inputValue}
-//         onChange={handleChange}
-//         type='number'
-//       />
-//     </div>
-//   );
-// }
-
 export default function FormatMoney(props: StringInputProps) {
-const {onChange, value = 'number', elementProps } = props
-const handleChange = useCallback(
-  (event: React.ChangeEvent<HTMLInputElement>) => onChange(event.currentTarget.value ? set(event.currentTarget.value) : unset()),[onChange]
-)
+  const { onChange, value: inputValue = '', elementProps } = props;
+
+  const handleChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const newValue = event.currentTarget.value.trim();
+      if (newValue === '') {
+        onChange(unset());
+      } else {
+        const parsedValue = parseInt(newValue, 10);
+        if (!isNaN(parsedValue)) {
+          onChange(set(parsedValue));
+        }
+      }
+    },
+    [onChange]
+  );
+
   return (
     <div>
-      <h2>
-        In US Dollars - {value ? formatMoney(Number(value) / 100) : null}
-      </h2>
+      <h2>In US Dollars - {inputValue ? formatMoney(Number(inputValue) / 100) : null}</h2>
       <p>Enter the amount, with cents and no decimal. It will auto convert above.</p>
       <input
         {...elementProps}
-        value={value}
+        value={inputValue}
         onChange={handleChange}
         type='number'
       />
     </div>
   );
 }
+
+// export default function FormatMoney(props: StringInputProps) {
+// const {onChange, value = 'number', elementProps } = props
+// const handleChange = useCallback(
+//   (event: React.ChangeEvent<HTMLInputElement>) => onChange(event.currentTarget.value ? set(event.currentTarget.value) : unset()),[onChange]
+// )
+//   return (
+//     <div>
+//       <h2>
+//         In US Dollars - {value ? formatMoney(Number(value) / 100) : null}
+//       </h2>
+//       <p>Enter the amount, with cents and no decimal. It will auto convert above.</p>
+//       <input
+//         {...elementProps}
+//         value={value}
+//         onChange={handleChange}
+//         type='number'
+//       />
+//     </div>
+//   );
+// }
 
 FormatMoney.displayName = 'FormatMoney';
